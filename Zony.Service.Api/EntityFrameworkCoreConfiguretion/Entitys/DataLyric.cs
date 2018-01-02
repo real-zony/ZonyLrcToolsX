ed39dc5db82bc31b60eb770c7f3fa93b4005e28a
Zony <zony@myzony.com>
2018-01-02 11:48:38 +0800

    add 对 API 项目新增 DataLyric 实体对象。

---
+++ Zony.Service.Api/EntityFrameworkCoreConfiguretion/Entitys/DataLyric.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zony.Service.Api.EntityFrameworkCoreConfiguretion.Entitys
+{
+    public class DataLyric
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Album { get; set; }
+        public string Artist { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string LyricText { get; set; }
+    }
+}
