f50b455c21f216c1b7eafced3a0614033304ca69
Zony <zony@myzony.com>
2018-01-17 22:37:50 +0800

    refactor 重构专辑图像下载插件。

--- Zony.Lib.AlbumDownLoad/Startup.cs
+++ Zony.Lib.AlbumDownLoad/Startup.cs
@@ -1,15 +1,15 @@
-﻿using Newtonsoft.Json.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Text;
 using Zony.Lib.Net;
 using Zony.Lib.Net.JsonModels.NetEase;
+using Zony.Lib.Net.JsonModels.NetEase.RequestModel;
 using Zony.Lib.Plugin.Attributes;
 using Zony.Lib.Plugin.Interfaces;
 using Zony.Lib.Plugin.Models;
 
 namespace Zony.Lib.AlbumDownLoad
 {
-    [PluginInfo("专辑图像下载插件", "Zony", "1.0.0.0", "http://www.myzony.com", "从网易云音乐下载专辑图像")]
+    [PluginInfo("专辑图像下载插件", "Zony", "1.0.1.0", "http://www.myzony.com", "从网易云音乐下载专辑图像")]
     public class Startup : IPluginAlbumDownloader, IPlugin
     {
         private readonly HttpMethodUtils m_netUtils = new HttpMethodUtils();
@@ -21,12 +21,17 @@ namespace Zony.Lib.AlbumDownLoad
             var _songModel = GetMusicInfoByNetease(info.Artist, info.Song);
             if (_songModel.id == 0) return false;
 
-            string _albumImg = GetAblumImageUrl(_songModel.id.ToString());
+            string _albumImg = GetAblumImageUrl(_songModel.id);
 
             imageData = new WebClient().DownloadData(_albumImg);
             return true;
         }
 
+        /// <summary>
+        /// 获取歌曲 SID 信息
+        /// </summary>
+        /// <param name="artist">艺术家</param>
+        /// <param name="songName">歌曲名</param>
         private NetEaseSongModel GetMusicInfoByNetease(string artist, string songName)
         {
             string _artist = m_netUtils.URL_Encoding(artist, Encoding.UTF8);
@@ -34,44 +39,27 @@ namespace Zony.Lib.AlbumDownLoad
             string _searchKey = $"{_artist}+{_title}";
 
             var _result = m_netUtils.Post<NetEaseResultModel>(url: @"http://music.163.com/api/search/get/web",
-                                                parameters: new
-                                                {
-                                                    csrf_token = string.Empty,
-                                                    s = _searchKey,
-                                                    type = 1,
-                                                    offset = 0,
-                                                    total = true,
-                                                    limit = 5
-                                                },
+                                                parameters: new NetEaseSearchRequestModel(_searchKey),
                                                 referer: "http://music.163.com",
                                                 mediaTypeValue: "application/x-www-form-urlencoded");
             return _result.result.songs[0];
         }
 
-        private string GetSID(string _resultStr)
-        {
-            JObject _jsonSid = JObject.Parse(_resultStr);
-            if (!_resultStr.Contains("result")) return null;
-            if (_jsonSid["result"]["songCount"].Value<int>() == 0)
-            {
-                return null;
-            }
-
-            JArray _jarraySID = (JArray)_jsonSid["result"]["songs"];
-            return _jarraySID[0]["id"].ToString();
-        }
-
-        private string GetAblumImageUrl(string sid)
+        /// <summary>
+        /// 获取专辑图像 URL 路径
+        /// </summary>
+        /// <param name="sid">歌曲 SID</param>
+        /// <returns></returns>
+        private string GetAblumImageUrl(int sid)
         {
             string _requestUrl = $"http://music.163.com/api/song/detail/";
-            string _songDetailJson = m_netUtils.Get(_requestUrl, new
+            var _result = m_netUtils.GetAsync<NetEaseSongDetailResultModel>(_requestUrl, new
             {
                 id = sid,
                 ids = $"%5B{sid}%5D"
-            });
+            }).Result;
 
-            JArray _songs = (JArray)JObject.Parse(_songDetailJson)["songs"];
-            return _songs[0]["album"]["picUrl"].ToString();
+            return _result?.songs?[0]?.album?.picUrl;
         }
     }
 }
---
+++ Zony.Lib.Net/JsonModels/NetEase/NetEaseSongDetailResultModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+namespace Zony.Lib.Net.JsonModels.NetEase
+{
+    /// <summary>
+    /// 歌曲详细信息 JSON 模型
+    /// </summary>
+    public class NetEaseSongDetailResultModel
+    {
+        /// <summary>
+        /// 状态码
+        /// </summary>
+        public string code { get; set; }
+
+        /// <summary>
+        /// 歌曲信息列表
+        /// </summary>
+        public List<NetEaseSongModel> songs { get; set; }
+    }
+}
