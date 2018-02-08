1263d1e3c5d3766b855c356ebc8957636c6c93b4
Zony <zony@myzony.com>
2018-02-08 21:58:50 +0800

    update

--- Zony.Lib.Plugin/Interfaces/IPluginAlbumDownloader.cs
+++ Zony.Lib.Plugin/Interfaces/IPluginAlbumDownloader.cs
@@ -2,8 +2,17 @@
 
 namespace Zony.Lib.Plugin.Interfaces
 {
+    /// <summary>
+    /// 专辑图像下载器
+    /// </summary>
     public interface IPluginAlbumDownloader
     {
+        /// <summary>
+        /// 下载专辑图像
+        /// </summary>
+        /// <param name="info">音乐信息</param>
+        /// <param name="imageData">下载完成的专辑图像数据流</param>
+        /// <returns>是否下载成功</returns>
         bool DownlaodAblumImage(MusicInfoModel info, out byte[] imageData);
     }
 }
--- Zony.Lib.Plugin/Interfaces/IPluginDownLoader.cs
+++ Zony.Lib.Plugin/Interfaces/IPluginDownLoader.cs
@@ -5,6 +5,12 @@
     /// </summary>
     public interface IPluginDownLoader
     {
+        /// <summary>
+        /// 下载歌词
+        /// </summary>
+        /// <param name="songName">歌曲名称</param>
+        /// <param name="artistName">歌手/艺术家</param>
+        /// <param name="data">输出的数据</param>
         void DownLoad(string songName, string artistName, out byte[] data);
     }
 }
