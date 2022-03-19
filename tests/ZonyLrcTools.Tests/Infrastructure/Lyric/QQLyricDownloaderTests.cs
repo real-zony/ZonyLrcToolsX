d250524208c140e9e68b3e0637a91b3b189155f7
real-zony <zony@myzony.com>
2022-03-19 11:42:00 +0800

    feat: QQ Music Downloader support dual language lyrics.

--- tests/ZonyLrcTools.Tests/Infrastructure/Lyric/QQLyricDownloaderTests.cs
+++ tests/ZonyLrcTools.Tests/Infrastructure/Lyric/QQLyricDownloaderTests.cs
@@ -25,5 +25,17 @@ namespace ZonyLrcTools.Tests.Infrastructure.Lyric
             lyric.ShouldNotBeNull();
             lyric.IsPruneMusic.ShouldBe(false);
         }
+        
+        // About the new feature mentioned in issue #87.
+        // Github Issue: https://github.com/real-zony/ZonyLrcToolsX/issues/87
+        [Fact]
+        public async Task DownloadAsync_Issue85_Test()
+        {
+            var lyric = await _lyricDownloader.DownloadAsync("Looking at Me", "Sabrina Carpenter");
+
+            lyric.ShouldNotBeNull();
+            lyric.IsPruneMusic.ShouldBeFalse();
+            lyric.ToString().ShouldContain("你好像快要不能呼吸");
+        }
     }
 }
