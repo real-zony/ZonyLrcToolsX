7463709a39dd62f5000449998ba7b88b390e1ae9
real-zony <zony@myzony.com>
2022-04-28 23:58:03 +0800

    feat: Deleted unused components.

--- src/ui/src/router/index.js
+++ src/ui/src/router/index.js
@@ -4,24 +4,12 @@ import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+const routes = [{
+    path: '/', name: 'home', component: HomeView
+}]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
