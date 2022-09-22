99fd3b50217729b93fc5d878964fcb05ea1a10a8
real-zony <zony@myzony.com>
2022-09-22 13:09:41 +0800

    ci: debug workflow.

--- src/ZonyLrcTools.Cli/publish.sh
+++ src/ZonyLrcTools.Cli/publish.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-Platforms=('win-x64' 'linux-x64' 'osx-x64')
+dotnet publish -r "linux-x64" -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
+# Platforms=('win-x64' 'linux-x64' 'osx-x64')
 
-if ! [ -d './TempFiles' ];
-then
-    mkdir ./TempFiles
-fi
+# if ! [ -d './TempFiles' ];
+# then
+#     mkdir ./TempFiles
+# fi
 
-rm -rf ./TempFiles/*
+# rm -rf ./TempFiles/*
 
-for platform in "${Platforms[@]}"
-do
-    dotnet publish -r "$platform" -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
+# for platform in "${Platforms[@]}"
+# do
+#     dotnet publish -r "$platform" -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
 
-    ls -a    
-    # cd ./bin/Release/net6.0/"$platform"/publish/ || exit
-    # zip -r ./ZonyLrcTools_"$platform"_"${VERSION}".zip ./
-    # cd ../../../../../
+#     ls -a    
+#     # cd ./bin/Release/net6.0/"$platform"/publish/ || exit
+#     # zip -r ./ZonyLrcTools_"$platform"_"${VERSION}".zip ./
+#     # cd ../../../../../
 
-    # mv ./bin/Release/net6.0/"$platform"/publish/ZonyLrcTools_"$platform"_"$Version".zip ./TempFiles
-done
+#     # mv ./bin/Release/net6.0/"$platform"/publish/ZonyLrcTools_"$platform"_"$Version".zip ./TempFiles
+# done
