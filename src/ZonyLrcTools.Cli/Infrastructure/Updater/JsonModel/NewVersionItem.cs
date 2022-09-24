6ad154a62b81e2012c87f7b0ab134b376ed3b1af
real-zony <zony@myzony.com>
2022-09-24 19:13:57 +0800

    feat: Add update check logic.

---
+++ src/ZonyLrcTools.Cli/Infrastructure/Updater/JsonModel/NewVersionItem.cs
@@ -0,0 +1,8 @@
+namespace ZonyLrcTools.Cli.Infrastructure.Updater.JsonModel;
+
+public class NewVersionItem
+{
+    public string Url { get; set; }
+
+    public NewVersionItemType ItemType { get; set; }
+}
---
+++ src/ZonyLrcTools.Cli/Infrastructure/Updater/JsonModel/NewVersionItemType.cs
@@ -0,0 +1,7 @@
+namespace ZonyLrcTools.Cli.Infrastructure.Updater.JsonModel;
+
+public enum NewVersionItemType
+{
+    Important,
+    NormalBinaryFile
+}
