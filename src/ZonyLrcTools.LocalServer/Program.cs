dbc1e44d2a8be6702ffd7d34ef6625a4b5fd7f4f
real-zony <zony@myzony.com>
2022-10-05 10:36:42 +0800

    Added WebSocket listener service.

--- src/ZonyLrcTools.LocalServer/Program.cs
+++ src/ZonyLrcTools.LocalServer/Program.cs
@@ -1,25 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
+using ZonyLrcTools.LocalServer;
+
+var app = RegisterAndConfigureServices();
+await ListenServices();
+
+async Task ListenServices()
+{
+    await new SuperSocketListener().ListenAsync();
+    await app?.RunAsync()!;
+}
+
+WebApplication? RegisterAndConfigureServices()
+{
+    var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+    builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+    var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseAuthorization();
+    app.MapControllers();
+
+    return app;
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ src/ZonyLrcTools.LocalServer/SuperSocketListener.cs
@@ -0,0 +1,15 @@
+﻿using SuperSocket.WebSocket.Server;
+
+namespace ZonyLrcTools.LocalServer;
+
+public class SuperSocketListener
+{
+    public async Task ListenAsync()
+    {
+        var host = WebSocketHostBuilder.Create()
+            .UseWebSocketMessageHandler(async (session, message) => { await session.SendAsync(message.Message); })
+            .Build();
+
+        await host.StartAsync();
+    }
+}
