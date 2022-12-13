bc5afad599217024be61b0ac2c2044d06a3242ed
real-zony <zony@myzony.com>
2022-12-13 22:54:19 +0800

    ci: Fixed the path problem

--- src/ZonyLrcTools.Cli/publish.sh
+++ src/ZonyLrcTools.Cli/publish.sh
@@ -12,9 +12,9 @@ for platform in "${Platforms[@]}"
 do
     dotnet publish -r "$platform" -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true || exit 1
 
-    cd ./bin/Release/net6.0/"$platform"/publish/ || exit 1
+    cd ./bin/Release/net7.0/"$platform"/publish/ || exit 1
     zip -r ./ZonyLrcTools_"$platform"_"${PUBLISH_VERSION}".zip ./ || exit 1
     cd ../../../../../ || exit 1
 
-    mv ./bin/Release/net6.0/"$platform"/publish/ZonyLrcTools_"$platform"_"$PUBLISH_VERSION".zip ./TempFiles
+    mv ./bin/Release/net7.0/"$platform"/publish/ZonyLrcTools_"$platform"_"$PUBLISH_VERSION".zip ./TempFiles
 done
