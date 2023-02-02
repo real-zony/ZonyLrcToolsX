0aa8ca5aef9c6b6ad5e8e381126c374ff6d5e6ee
real-zony <zony@myzony.com>
2023-02-02 22:47:30 +0800

    docs: Improve the help content.

--- src/ZonyLrcTools.Cli/Commands/ToolCommandBase.cs
+++ src/ZonyLrcTools.Cli/Commands/ToolCommandBase.cs
@@ -3,7 +3,7 @@ using McMaster.Extensions.CommandLineUtils;
 
 namespace ZonyLrcTools.Cli.Commands
 {
-    [HelpOption("--help|-h", Description = "欢迎使用 ZonyLrcToolsX Command Line Interface。")]
+    [HelpOption("--help|-h", Description = "欢迎使用 ZonyLrcToolsX Command Line Interface，有任何问题请访问 https://soft.myzony.com 或添加 QQ 群 337656932 寻求帮助。")]
     public abstract class ToolCommandBase
     {
         protected virtual Task<int> OnExecuteAsync(CommandLineApplication app)
